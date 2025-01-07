d8d594c8428917c87ad3b01c77ed2448be12807e
ciriandev <rar1602@gmail.com>
2025-01-07 21:47:09 +0100

    Retoques

--- BibliotecaAPI/obj/Debug/net9.0/BibliotecaAPI.AssemblyInfo.cs
+++ BibliotecaAPI/obj/Debug/net9.0/BibliotecaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3610cf41b7bcaca52e20b611c32a02f3e85152b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56d10746006c437b26bf5edccb9358a5420ee67")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
